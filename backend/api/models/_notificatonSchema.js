cb33da11eb5155b16e7d49867cefe550c7807360
Christoph Suessmeier <mail@suessmeier.net>
2023-09-20 09:50:14 +0200

    Added notification and service schema

--- backend/api/models/_notificatonSchema.js
+++ backend/api/models/_notificatonSchema.js
@@ -3,8 +3,18 @@ import Subscription from "./_subscriptionSchema.js";
 
 const Schema = mongoose.Schema;
 
-const notificationSchema = new Schema({
-  // TODO: Implement
-});
+const notificationSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    type: { type: String, required: true }, // usage, cancel?
+    sent: { type: Boolean, required: true, default: false },
+    subscriptionId: {
+      type: Schema.Types.ObjectId,
+      ref: Subscription,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model("Notification", notificationSchema);
