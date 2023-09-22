4151d5b771a1c29bf1b35f5bc85c5ad4fe13ac65
Christoph Suessmeier <mail@suessmeier.net>
2023-09-22 13:41:07 +0200

    Implemented DataContext, hook to fetch all data and EventEmitter to request refetch from anywhere

---
+++ frontend/src/hooks/useDataFetching.js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import { useDataContext } from "../contexts/dataContext";
+import useCategory from "./useCategory";
+import useDashboard from "./useDashboard";
+import useSubscription from "./useSubscription";
+import useSubscriptionUsage from "./useSubscriptionUsage";
+
+export default function useDataFetching() {
+  // ---- Data Context ----
+  const {
+    setSubscriptions,
+    setAllCategories,
+    setUsedCategories,
+    setUsages,
+    setDashboardData,
+  } = useDataContext();
+
+  // ---- Data Fetching ----
+  const { getAllSubscriptions } = useSubscription();
+  const { getAllCategories } = useCategory();
+  const { getDashboardData } = useDashboard();
+  const { getAllSubscriptionUsage } = useSubscriptionUsage();
+
+  // ---- State ----
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [allCategoriesFetched, setAllCategoriesFetched] = useState(false);
+
+  // Fetch / Refetch function ----
+  async function fetchData(abortController) {
+    setLoading(true);
+    setError(false);
+    setErrorMessage("");
+
+    // Fetch all the required data
+    async function fetchRequiredData() {
+      try {
+        console.log("Stargin fetch");
+        const [fetchedSubscriptions, fetchedDashboardData, fetchedUsages] =
+          await Promise.all([
+            getAllSubscriptions(abortController),
+            getDashboardData(abortController),
+            getAllSubscriptionUsage(abortController),
+          ]);
+
+        console.log("Fetch done, proceeding...");
+        console.log("fetchedSubscriptions", fetchedSubscriptions);
+
+        // build used categories array
+        if (!fetchedSubscriptions) {
+          throw new Error(
+            "Error loading subscriptions, please reload the page to try again",
+          );
+        }
+
+        console.log("Building used categories array");
+        const fetchedUsedCategories = fetchedSubscriptions?.reduce(
+          (prev, curr) => {
+            if (!prev.some((category) => category._id === curr.category._id)) {
+              prev.push(curr.category);
+            }
+
+            return prev;
+          },
+          [],
+        );
+
+        // write data to context
+        setSubscriptions(fetchedSubscriptions);
+        setDashboardData(fetchedDashboardData);
+        setUsedCategories(fetchedUsedCategories);
+        setUsages(fetchedUsages);
+      } catch (error) {
+        setError(true);
+        setErrorMessage(error.message);
+      }
+    }
+    await fetchRequiredData();
+
+    // Fetch all categories once if needed
+    async function fetchAllCategories() {
+      try {
+        const fetchedCategories = await getAllCategories(abortController);
+
+        setAllCategoriesFetched(true);
+        setAllCategories(fetchedCategories);
+      } catch (error) {
+        setError(true);
+        setErrorMessage(error.message);
+      }
+    }
+
+    if (!allCategoriesFetched) {
+      await fetchAllCategories();
+    }
+
+    // ...aaaaaaand we're done
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    // Probably not the best way, but let's use one abort controller for all fetching
+    const abortController = new AbortController();
+
+    fetchData(abortController);
+
+    return () => abortController.abort();
+  }, []);
+
+  return { loading, error, errorMessage, refetchData: fetchData };
+}
---
+++ frontend/src/utils/EventEmitter.js
@@ -0,0 +1,41 @@
+// simple event emitter class to allow components to communicate with each other
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, callback) {
+    // if event 'eventName' doesn't exist, create it
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    // attach the provided callback to the event
+    this.events[eventName].push(callback);
+  }
+
+  off(eventName, callback) {
+    // if this event doesnt exist, do nothing
+    if (!this.events[eventName]) return;
+
+    // otherwise, remove the callback from the event
+    const eventIndex = this.events[eventName].indexOf(callback);
+    if (eventIndex > -1) {
+      this.events[eventName].splice(eventIndex, 1);
+    }
+  }
+
+  emit(eventName, ...args) {
+    // if this event doesnt exist, do nothing
+    if (!this.events[eventName]) return;
+
+    // otherwise, call all callbacks attached to the event
+    this.events[eventName].forEach((callback) => {
+      callback(...args);
+    });
+  }
+}
+
+// create a singleton instance of the event emitter and export it
+const emitter = new EventEmitter();
+export default emitter;
